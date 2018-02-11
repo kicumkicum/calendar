cd2f5a0e94aa976ac6c327ad9c0a52e5f274d3e7
kicumkicum <kicumkicum@gmail.com>
2018-02-12 02:51:52 +0700

    Add component Date

---
+++ src/components/date.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+/**
+ * @param {{
+ *      date: Date,
+ *      locale: string
+ * }} props
+ */
+export default (props) => {
+	const year = props.date.getFullYear();
+	let month = props.date.toLocaleString(props.locale, { month: 'long' });
+	month = month[0].toUpperCase() + month.substr(1);
+
+	return (
+		<span class='month-year' id='label'> {`${month} ${year}`} </span>
+	);
+};
